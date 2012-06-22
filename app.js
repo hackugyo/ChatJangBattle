ebbf40e3e62bab368208fee3a8bf9ca923860364
kwatanabe <kwatanabe@kbmj.com>
2012-06-22 13:57:02 +0900

    portはforemanと統一して5000に

--- app.js
+++ app.js
@@ -147,7 +147,7 @@ app.get('/socket', function(req, res) {
   res.render('socketio', {title:'Socket.IO DEMO'});
 });
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 5000;
 app.listen(port, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });

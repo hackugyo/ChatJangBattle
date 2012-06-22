ae97ed1b82c8a3fdd5fcf66bac3c2ac937d3b23a
kwatanabe <kwatanabe@kbmj.com>
2012-06-22 12:19:54 +0900

    ポート番号の設定を追加．

--- app.js
+++ app.js
@@ -135,6 +135,7 @@ app.get('/socket', function(req, res) {
   res.render('socketio', {title:'Socket.IO DEMO'});
 });
 
-app.listen(3000, function(){
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
